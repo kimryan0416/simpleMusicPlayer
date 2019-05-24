8d22bb360e112e9fbb44eb8fae4af52fa87993bf
rk494 <rk494@cornell.edu>
2019-05-24 14:12:35 +0900

    Added preliminary scaffolding for Node.JS/Express/Pug integration

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const indexRouter = require('./routes/index');
+
+app.set('view engine','pug');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/',indexRouter);
+
+// Setting up the server to port 7000
+const server = app.listen(7000,()=>{
+	console.log(`Express running => PORT ${server.address().port}`);
+});
+
+module.exports = app;
